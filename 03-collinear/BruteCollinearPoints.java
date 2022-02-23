2eb83053d0f1782ff1a9e33b032409041d1f368f
Chen Zhou <chenzhoux93@gmail.com>
2022-02-23 08:45:16 -0500

    PA03

---
+++ 03-collinear/BruteCollinearPoints.java
@@ -0,0 +1,28 @@
+/* *****************************************************************************
+ *  Name: Chen Zhou
+ *  Date: 02/23/2022
+ *  Description: BruteCollinearPoints Implementation
+ **************************************************************************** */
+
+public class BruteCollinearPoints {
+    private int numOfSegments = 0;
+
+    // finds all line segments containing 4 points
+    public BruteCollinearPoints(Point[] points) {
+
+    }
+
+    // the number of line segments
+    public int numberOfSegments() {
+        return numOfSegments;
+    }
+
+    // the line segments
+    public LineSegment[] segments() {
+
+    }
+
+    public static void main(String[] args) {
+
+    }
+}
